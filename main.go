bc2ab943f7a3acb338adc4a9bfc53f036ed3d41d
Edward Pie <ep@hellofresh.com>
2018-09-09 21:48:38 +0200

    adds status endpoint and handles graceful shutdown of server

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
 	"go.uber.org/zap"
@@ -41,4 +48,70 @@ func main() {
 	}
 
 	logger.Info("env vars and logger initialized successfully")
+
+	interval := (24 * time.Hour) / time.Duration(env.SyncFrequency)
+	ticker := time.NewTicker(interval)
+
+	go func() {
+		for now := range ticker.C {
+			logger.Info("starting to process unprocessed flight records")
+
+			processFlightRecords()
+			elapsed := time.Since(now)
+			logger.Info("finished processing unprocessed flight records", zap.Any("in", elapsed))
+		}
+	}()
+
+	listener, err := net.Listen("tcp4", fmt.Sprintf(":%d", env.Port))
+	if err != nil {
+		logger.Fatal("failed binding to port",
+			zap.Int("port", env.Port),
+			zap.Error(err),
+		)
+	}
+
+	defer listener.Close()
+
+	url := fmt.Sprintf("http://%s/", listener.Addr())
+	logger.Info("listening on:",
+		zap.String("url", url),
+	)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	srv := http.Server{
+		Handler: mux,
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		defer close(idleConnsClosed)
+		recv := <-sigs
+		logger.Info("received signal, shutting down", zap.String("signal", recv.String()))
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			logger.Warn("error shutting down server", zap.Error(err))
+		}
+	}()
+
+	if err := srv.Serve(listener); err != nil {
+		if err != http.ErrServerClosed {
+			logger.Fatal("http.Serve returned an error",
+				zap.Error(err),
+			)
+		}
+	}
+	<-idleConnsClosed
+}
+
+func processFlightRecords() {
+
 }
