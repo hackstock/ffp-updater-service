07cf86bd6401137d084c613c9457d48c04e51de6
Edward Pie <ep@hellofresh.com>
2018-10-14 16:41:28 +0200

    adds method for fetching unprocessed flight records

--- pkg/rewards_process.go
+++ pkg/rewards_process.go
@@ -1,12 +1,18 @@
 package pkg
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"go.uber.org/zap"
 )
 
-// FlightRecord ...
+// FlightRecord represent a FFP user's flight record
+// to be used for calculating points to be awared
 type FlightRecord struct {
 	AyTax                string `json:"ayTax"`
 	BaseFare             string `json:"baseFare"`
@@ -46,6 +52,13 @@ type FlightRecord struct {
 	IsProcessed          bool   `json:"isProcessed"`
 }
 
+// FRResponse is a struct for holding API response
+// from endpoint for fetching unprocessed flight records
+type FRResponse struct {
+	Status int             `json:"status"`
+	Data   []*FlightRecord `json:"data"`
+}
+
 // RewardsProcess ...
 type RewardsProcess struct {
 	host   string
@@ -65,4 +78,30 @@ func NewRewardsProcess(host string, client *http.Client, logger *zap.Logger) *Re
 // Run ...
 func (p *RewardsProcess) Run() {
 
+}
+
+func (p *RewardsProcess) getUnprocessedFlightRecords() (*FRResponse, error) {
+	url := fmt.Sprintf("%s/ffp/flightrecords/unprocessed", p.host)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusOK {
+		frRes := new(FRResponse)
+		err = json.NewDecoder(res.Body).Decode(frRes)
+		if err != nil {
+			return nil, err
+		}
+		return frRes, nil
+	}
+
+	_, _ = io.Copy(ioutil.Discard, res.Body)
+	return nil, errors.New(res.Status)
 }
